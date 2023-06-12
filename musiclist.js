689613dfea598b561931548db3897ffdea8dfd16
Gayatri-projects <notesgayatri25@gmail.com>
2023-06-12 16:55:31 +0530

    Initial commit

---
+++ musiclist.js
@@ -0,0 +1,46 @@
+let allMusic = [
+    {
+      name: "People",
+      artist: "-Libianca",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Until i found you",
+      artist: "Stephen-Sanchez",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Love me like you do",
+      artist: "Ellie-Goulding",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Play date",
+      artist: "Melanie-Martinez",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Baby",
+      artist: "Justin-Bieber",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Kuthu fire",
+      artist: "Vidya-Vox",
+      img: "music-7",
+      src: "music-7"
+    },
+    {
+        name: "Stereo Hearts",
+        artist: "Adam-Levine",
+        img: "music-6",
+        src: "music-6"
+      },
+
+    
+  ];
